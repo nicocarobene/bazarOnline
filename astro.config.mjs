f91c311af394d08e3133d4afae0a5a4eb183fb28
Nicolas <nicolascarobene1@gmail.com>
2023-09-24 15:19:05 +0200

    (fix) add adapter for deplay at vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import preact from '@astrojs/preact'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import preact from '@astrojs/preact';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), preact()]
-})
+  integrations: [tailwind(), preact()],
+  adapter: vercel()
+});
